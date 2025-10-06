8a9ea02205e7ecc49ca43ede87175383f6d71e43
juhuntenburg <ju.huntenburg@gmail.com>
2025-10-06 15:52:15 +0200

    Editing user possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  # app/controllers/users_controller.rb
   def create
     @user = User.new(user_params)
 
@@ -12,6 +11,19 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
